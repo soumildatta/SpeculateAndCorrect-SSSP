9f58bef51e860be4238bb39de8a25ad76cce4d69
Soumil Datta <soumildatta@gmail.com>
2021-11-05 10:32:45 -0500

    Initialize pools

--- parallelSpecAndCorr/main.cpp
+++ parallelSpecAndCorr/main.cpp
@@ -3,6 +3,7 @@
 #include "tEdgeList.h"
 #include "tTimer.h"
 #include "tData.h"
+#include "tPool.h"
 
 #include <pthread.h>
 #include <thread>
@@ -14,7 +15,7 @@ using std::printf;
 // Define max threads for device
 #define maxThreads thread::hardware_concurrency()
 
-tGraph processGraph(path &filename);
+tGraph processGraph(const path &filename);
 
 void *Testing(void *threadid);
 
@@ -30,18 +31,18 @@ int main(int argc, char *argv[])
 	path filename { argv[1] };
 	path verifyFilename { argv[2] };
 	int iterations { atoi(argv[3]) };
+	int sourceNode { atoi(argv[4]) };
 
 	cout << "Max threads supported by this system: " << maxThreads << endl;
 
 	// Testing threads
 	pthread_t threads[maxThreads];
 
-	// Struct to be passed as argument list
-	struct tData *data = (struct tData *)malloc(sizeof(struct tData));
-
-
 	// Process graph
 	tGraph graph { processGraph(filename) };
+
+	// Struct to be passed as argument list
+	struct tData *data = (struct tData *)malloc(sizeof(struct tData));
 
 	// Initialize struct
 	data->nodes = graph.nodes;
@@ -49,13 +50,22 @@ int main(int argc, char *argv[])
 
 	const nodeCost initialNodeCost(~0u, INT32_MAX);
 	vector<nodeCost> solution(graph.nNodes, initialNodeCost);
+	solution[sourceNode] =  nodeCost(sourceNode, 0);
 	data->solution = solution;
 
-//	vector<uint32_t> speculationPool(graph.nNodes, ~0u);
-//	data->speculationPool = speculationPool;
-//
-//	vector<uint32_t> correctionPool(graph.nNodes, ~0u);
-//	data->correctionPool = correctionPool;
+	// Initialize speculation pool with source node cost initialized to 0
+	vector<uint32_t> speculationPool(graph.nNodes, ~0u);
+	data->speculationPool.pool = speculationPool;
+	data->speculationPool.removeIndex = 0u;
+	data->speculationPool.addIndex = 1u;
+	data->speculationPool.bufferSize = speculationPool.size();
+
+	// Initialize correction pool that is empty
+	vector<uint32_t> correctionPool(graph.nNodes, ~0u);
+	data->correctionPool.pool = correctionPool;
+	data->correctionPool.removeIndex = 0u;
+	data->correctionPool.addIndex = 0u;
+	data->correctionPool.bufferSize = correctionPool.size();
 
 	data->source = 0u;
 
@@ -108,7 +118,7 @@ void *Testing(void *threadid) {
    pthread_exit(NULL);
 }
 
-tGraph processGraph(path &filename)
+tGraph processGraph(const path &filename)
 {
 	cout << "Processing Graph" << endl;
 
