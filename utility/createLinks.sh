f6465324082cf95676278a67633217230af690f3
Soumil Datta <soumildatta@gmail.com>
2021-11-22 23:16:48 -0600

    Create performance metrics timesheet:

--- utility/createLinks.sh
+++ utility/createLinks.sh
@@ -6,3 +6,5 @@ ln -s ../shared/tTimer.h ./tTimer.h
 
 ln -s ../shared/validation.h ./validation.h
 ln -s ../shared/validation.cpp ./validation.cpp
+
+ln -s ../shared/performanceMetrics.h ./performanceMetrics.h
---
+++ utility/performanceMetrics.h
@@ -0,0 +1,1 @@
+../shared/performanceMetrics.h
